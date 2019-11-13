bb27bc0901dec3b7dfd373c248c6f86be17149a3
Crystal Danger <cdneal091@gmail.com>
2019-11-12 21:10:28 -0700

    ## style improvements considerations for responsive design

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { A } from 'hookrouter'
+import React, { useState } from 'react'
+import { A , usePath } from 'hookrouter'
+import useMediaQuery from 'use-media-query-hook'
 
 export default props => {
+  const path = usePath()
+  const isSmall = useMediaQuery('(max-width: 600px)')
   const listBoards = () => {
     return Object.keys(props.boardList).map(board => {
       return <A key={board} className='board-link' href={`/${board}`}>{board}</A>})
   }
-  return (
-    <ul className='board-list'>
+  const fullList = () => {
+    return (
+      <ul className='board-list'>
       {listBoards()}
       <A className='board-link' href='/'>home</A>
-    </ul>
-  )
+      </ul>
+    )
+  }
+  const [open, setOpen] = useState(false)
+  const compactList = () => {
+    return (
+      <div>
+        Im a compact list!
+      </div>
+    )
+  }
+  return isSmall ? compactList() : fullList()
 }
